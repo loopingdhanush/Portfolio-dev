54c9005bc51c8b0e6115bb1fd9ab7b01b90d69b4
Dhanush S <dhanush.utility@gmail.com>
2025-02-16 22:03:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import express from "express"
+import express from "express";
+import { createServer } from "http";
+
 const app = express();
 
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: "public" });
 });
+
+export default app;
+
 
 app.listen(5000, () => console.log("Server ready on port 5000."));
 
